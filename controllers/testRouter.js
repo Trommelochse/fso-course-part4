1b0c651ae11c2f7ed68a9c3993f19b2c64911cfa
Clemens Janes <clemens.a.janes@gmail.com>
2023-10-21 00:16:12 +0300

    Exercise 5.18

--- controllers/testRouter.js
+++ controllers/testRouter.js
@@ -1,8 +1,10 @@
 const testController = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 testController.post('/reset', async (request, response) => {
   await Blog.deleteMany({})
+  await User.deleteMany({})
   response.sendStatus(204)
 })
 
